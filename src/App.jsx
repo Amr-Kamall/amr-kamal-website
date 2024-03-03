0175b89bfc7530c97ee5ac7ee2d2537543eb2767
amr <amr442028@gmail.com>
2024-03-04 00:19:51 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
 import Header from "./components/1-header/Header";
 import Hero from "./components/2-hero/Hero";
 import Main from "./components/3-main/Main";
 import Contact from "./components/4-contact/Contact";
 import Footer from "./components/5-footer/Footer";
+import { Crisp } from "crisp-sdk-web";
 
 function App() {
+  useEffect(() => {
+    // Set Crisp website ID dynamically
+    // const meta = document.createElement("meta");
+    // meta.name = "crisp-site-verification";
+    // meta.content = "42db37e0-15c9-48d0-97e7-22af755dcf02";
+    // document.head.appendChild(meta);
+
+    Crisp.configure("42db37e0-15c9-48d0-97e7-22af755dcf02");
+
+    // Clean up function
+    return () => {
+      // Remove meta tag
+      // document.head.removeChild(meta);
+    };
+  }, []);
   return (
     <div className="container">
       <Header />
