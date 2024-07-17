776a82b1f678ce21ae81ef9263f596bdd422f2f1
amr <amrmohamedkamall1@gmail.com>
2024-07-18 02:07:22 +0300

    update projects

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,17 +1,28 @@
 export const myProjects = [
   {
-    projectTitle: "Fast Pizza",
-    category: "react",
-    imgPath: "/images/pizza.png",
-    liveDemo: "https://fast-pizza-roan.vercel.app/",
-    githubRepo: "https://github.com/Amr-Kamall/fast-pizza",
-  },
-  {
     projectTitle: "Natours",
     category: "html&css",
     imgPath: "/images/natours.png",
     liveDemo: "https://natours-olive-five.vercel.app/",
     githubRepo: "https://github.com/Amr-Kamall/Natours",
+    info:"Natours is a beautifully designed travel website built with HTML and CSS"
+  },
+  {
+    projectTitle: "E-commerce",
+    category: "react",
+    imgPath: "/images/ecommerce.png",
+    liveDemo: "https://e-commerce-rosy-chi.vercel.app/",
+    githubRepo: "https://github.com/Amr-Kamall/Ecommerce",
+    info:"This e-commerce website offers a seamless shopping experience"
+  },
+
+  {
+    projectTitle: "Dashboard",
+    category: "react",
+    imgPath: "/images/dashboard.png",
+    liveDemo: "https://dashboard-bay-chi.vercel.app/",
+    githubRepo: "https://github.com/Amr-Kamall/dashboard",
+    info:"The dashboard project is comprehensive web application with react.js"
   },
   {
     projectTitle: "watches",
@@ -19,13 +30,16 @@ export const myProjects = [
     imgPath: "/images/Watches.png",
     liveDemo: "https://watches-shopping.vercel.app/",
     githubRepo: "https://github.com/Amr-Kamall/watches-shopping",
+    info:"The watches shopping website is stylish online store built with HTML, CSS, and Js."
   },
+
   {
     projectTitle: "coffee shop",
     category: "html&css",
     imgPath: "/images/Coffee.png",
     liveDemo: "https://coffee-ashen-iota.vercel.app/",
     githubRepo: "https://github.com/Amr-Kamall/coffee-shop",
+    info:"The coffee shop website is a visually appealing and user-friendly"
   },
   {
     projectTitle: "World Wise",
@@ -33,6 +47,7 @@ export const myProjects = [
     imgPath: "/images/WorldWise.png",
     liveDemo: "https://world-wise-ruby.vercel.app",
     githubRepo: "https://github.com/Amr-Kamall/World-Wise",
+    info:"World Wise is an interactive  exploration platform built with React and Mapbox"
   },
   {
     projectTitle: "movies",
@@ -40,13 +55,7 @@ export const myProjects = [
     imgPath: "/images/movie.png",
     liveDemo: "https://use-pop-corn-zeta.vercel.app/",
     githubRepo: "https://github.com/Amr-Kamall/UsePopcorn",
-  },
-  {
-    projectTitle: "Dashboard",
-    category: "react",
-    imgPath: "/images/dashboard.png",
-    liveDemo: "https://dashboard-bay-chi.vercel.app/",
-    githubRepo: "https://github.com/Amr-Kamall/dashboard",
+    info:"The movies website is an engaging platform built with React"
   },
 
   {
@@ -55,21 +64,15 @@ export const myProjects = [
     imgPath: "/images/crud.png",
     liveDemo: "https://crud-hazel-delta.vercel.app/",
     githubRepo: "https://github.com/Amr-Kamall/Crud",
+    info:"web application with JS enables users to create, read, update, and delete data"
   },
-  {
-    projectTitle: "Nike",
-    category: "javascript",
-    imgPath: "/images/Nike.png",
-    liveDemo: "https://nike-store-henna.vercel.app/",
-    githubRepo: "https://github.com/Amr-Kamall/Nike-Store",
-  },
-
   {
     projectTitle: "Covid-19",
     category: "bootstrab",
     imgPath: "/images/covid.png",
     liveDemo: "https://covid-19-bay.vercel.app/",
     githubRepo: "https://github.com/Amr-Kamall/covid-19",
+    info:"COVID-19 website is visually appealing platform built with Bootstrap."
   },
   {
     projectTitle: "Level",
@@ -77,5 +80,6 @@ export const myProjects = [
     imgPath: "/images/Level.png",
     liveDemo: "https://level-company.vercel.app/",
     githubRepo: "https://github.com/Amr-Kamall/level-company",
+    info:"The Level website is a sleek and modern platform created using bootstrap."
   },
 ];
